6e6c49fec7781e96ee32883de0926f954606912a
mohammed98k <sun.mohamed17@gmail.com>
2023-07-29 21:06:29 +0300

    commit

---
+++ Task-1.py
@@ -0,0 +1,9 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+dob = input("Please enter your date of birth: ")
+
+print("Name:", name)
+print("Age:", age)
+print("Date of Birth:", dob)
+
+
--- task1.py
+++
@@ -1,2 +0,0 @@
-input('name,age,birthday-date:')
-print(input)
