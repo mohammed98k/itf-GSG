c77bd0c88e2cf5c1926d095f9946bfb33cc1fae0
mohammed98k <76209805+mohammed98k@users.noreply.github.com>
2023-08-04 14:57:03 +0300

    Add files via upload

---
+++ functions-1.py
@@ -0,0 +1,29 @@
+n1 = int(input("n1: "))
+n2 = int(input("n2: "))
+def sum(n1,n2):
+    total = n1+n2
+    print ("sum:",total)
+
+sum(n1,n2)
+
+def subtraction(n1,n2):
+    sub = n1-n2
+    print ("sub:",sub)
+
+subtraction(n1,n2)
+
+def division(n1,n2):
+    total = n1/n2
+    print ("total:",total)
+
+division(n1,n2)
+
+
+def multiply(n1,n2):
+    total = n1*n2
+    print ("total:",total)
+
+multiply(n1,n2)
+
+
+
