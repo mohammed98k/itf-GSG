82cfd8631ea43faf689b7f610a13db7d0847c684
mohammed98k <76209805+mohammed98k@users.noreply.github.com>
2023-08-05 22:24:41 +0300

    Add files via upload

---
+++ function discussion.py
@@ -0,0 +1,20 @@
+def triangle():
+     x = int(input("base lingth:"))
+     y = int(input("hight:"))
+     a = int(input("lingth 2:"))
+     b = int(input("lingth 3:"))
+     area = (x * y)/2
+     perimeter = x + a + b
+     print("area:" , area , "perimeter:" , perimeter)
+
+triangle()
+
+def square():
+    z = int(input("lingth:"))
+    area = (z * z)
+    perimeter = z * 4
+    print("area:", area , "perimeter:", perimeter)
+
+square()
+
+
